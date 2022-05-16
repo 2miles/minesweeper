d70cd7af09d27afee1287bb9b5bde9f37dfc6420
miles <miles4599@gmail.com>
2022-05-16 15:22:31 -0700

    refactored code into Game class

--- game.py
+++ game.py
@@ -8,14 +8,6 @@ from debug import debug
 
 
 # Create funtion to draw texts
-
-pygame.init()
-display = pygame.display.set_mode((vars.SCREEN_W, vars.SCREEN_H))  # Create display
-pygame.display.set_caption("Minesweeper")
-clock = pygame.time.Clock()  # create timer
-
-
-# Create funtion to draw texts
 def drawText(txt, s):
     screen_text = pygame.font.SysFont("Calibri", s, True).render(
         txt, True, (255, 255, 255)
@@ -27,107 +19,116 @@ def drawText(txt, s):
     return surface
 
 
-def gameLoop():
-    gameState = "Playing"  # Game state
-    seconds = 0
-    final_score = 0
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        self.display = pygame.display.set_mode(
+            (vars.SCREEN_W, vars.SCREEN_H)
+        )  # Create display
+        pygame.display.set_caption("Minesweeper")
+        self.clock = pygame.time.Clock()  # create timer
 
-    background = Background()
-    grid = Grid(vars.ROWS, vars.COLS, vars.MINES)
-    timer = Status(vars.TIMER_LOC_X, vars.TIMER_LOC_Y)
-    remaining = Status(vars.TIMER_LOC_Y, vars.TIMER_LOC_Y)
-    faces = Faces(vars.FACE_LOC_X, vars.FACE_LOC_Y)
+    def game_loop(self):
 
-    while gameState != "Exit":
-        # Reset screen
-        clock.tick(60)  # Tick fps
-        seconds += 1
-        if seconds % 60 == 0:
-            if gameState == "Playing":
-                timer.update(seconds // 60)
-                final_score = seconds // 60
-        remaining.update(grid.mines_left)
+        self.seconds = 0
+        self.final_score = 0
+        self.game_state = "Playing"
 
-        for event in pygame.event.get():
-            # Check if player close window
-            if event.type == pygame.QUIT:
-                gameState = "Exit"
+        self.background = Background()
+        self.grid = Grid(vars.ROWS, vars.COLS, vars.MINES)
+        self.timer = Status(vars.TIMER_LOC_X, vars.TIMER_LOC_Y)
+        self.remaining = Status(vars.TIMER_LOC_Y, vars.TIMER_LOC_Y)
+        self.faces = Faces(vars.FACE_LOC_X, vars.FACE_LOC_Y)
 
-            # If game is over, user can press 'r' to restart
-            if gameState == "Game Over" or gameState == "Win":
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_r:
-                        gameState = "Exit"
-                        gameLoop()
-            else:
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    gameState = "Mouse down"
-                elif event.type == pygame.MOUSEBUTTONUP:
-                    gameState = "Playing"
-                    for line in grid.boxes:
-                        for box in line:
-                            if box.rect.collidepoint(event.pos):
-                                if event.button == 1:
-                                    # If player left clicked on the box
-                                    grid.revealGrid(box.x, box.y)
-                                    # Toggle flag off
-                                    if box.flag:
-                                        grid.mines_left += 1
-                                        box.flag = False
-                                    # If it's a mine
-                                    if box.val == -1:
-                                        gameState = "Game Over"
-                                        box.mineClicked = True
-                                elif event.button == 3:
-                                    # If the player right clicked
-                                    if not box.clicked:
+        while self.game_state != "Exit":
+            # Reset screen
+            self.clock.tick(60)  # Tick fps
+            self.seconds += 1
+            if self.seconds % 60 == 0:
+                if self.game_state == "Playing":
+                    self.timer.update(self.seconds // 60)
+                    self.final_score = self.seconds // 60
+            self.remaining.update(self.grid.mines_left)
+
+            for event in pygame.event.get():
+                # Check if player close window
+                if event.type == pygame.QUIT:
+                    self.game_state = "Exit"
+
+                # If game is over, user can press 'r' to restart
+                if self.game_state == "Game Over" or self.game_state == "Win":
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_r:
+                            self.game_state = "Exit"
+                            self.game_loop()
+                else:
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        self.game_state = "Mouse down"
+                    elif event.type == pygame.MOUSEBUTTONUP:
+                        self.game_state = "Playing"
+                        for line in self.grid.boxes:
+                            for box in line:
+                                if box.rect.collidepoint(event.pos):
+                                    if event.button == 1:
+                                        # If player left clicked on the box
+                                        self.grid.revealGrid(box.x, box.y)
+                                        # Toggle flag off
                                         if box.flag:
+                                            self.grid.mines_left += 1
                                             box.flag = False
-                                            grid.mines_left += 1
-                                        else:
-                                            box.flag = True
-                                            grid.mines_left -= 1
+                                        # If it's a mine
+                                        if box.val == -1:
+                                            self.game_state = "Game Over"
+                                            box.mineClicked = True
+                                    elif event.button == 3:
+                                        # If the player right clicked
+                                        if not box.clicked:
+                                            if box.flag:
+                                                box.flag = False
+                                                self.grid.mines_left += 1
+                                            else:
+                                                box.flag = True
+                                                self.grid.mines_left -= 1
 
-        # check for win
-        if gameState != "Exit":
-            if grid.check_for_win():
-                gameState = "Win"
+            # check for win
+            if self.game_state != "Exit":
+                if self.grid.check_for_win():
+                    self.game_state = "Win"
 
-        # render stuff to the screen
-        display.blit(background.draw(), (0, 0))
-        display.blit(faces.draw(gameState), (vars.FACE_LOC_X, vars.FACE_LOC_Y))
-        display.blit(timer.draw(), (timer.rect))
-        display.blit(remaining.draw(), (remaining.rect))
-        display.blit(grid.draw(), (grid.rect))
-        if gameState == "Win":
-            display.blit(drawText("You Won!", 50), (vars.BORDER, vars.SCREEN_H // 2))
-            display.blit(
-                drawText(f"Your Score is {final_score}", 50),
-                (vars.BORDER, vars.SCREEN_H // 2 + 50),
+            # render stuff to the screen
+            self.display.blit(self.background.draw(), (0, 0))
+            self.display.blit(
+                self.faces.draw(self.game_state), (vars.FACE_LOC_X, vars.FACE_LOC_Y)
             )
-            display.blit(
-                drawText("R to restart", 50),
-                (vars.BORDER, vars.SCREEN_H // 2 + 100),
-            )
-        if gameState == "Game Over":
-            display.blit(
-                drawText("Game over!", 50), (vars.SCREEN_W // 2, vars.SCREEN_H // 2)
-            )
-            display.blit(
-                drawText("R to restart", 50),
-                (vars.SCREEN_W // 2, vars.SCREEN_H // 2 + 50),
-            )
+            self.display.blit(self.timer.draw(), (self.timer.rect))
+            self.display.blit(self.remaining.draw(), (self.remaining.rect))
+            self.display.blit(self.grid.draw(), (self.grid.rect))
+            if self.game_state == "Win":
+                self.display.blit(
+                    drawText("You Won!", 50), (vars.BORDER, vars.SCREEN_H // 2)
+                )
+                self.display.blit(
+                    drawText(f"Your Score is {self.final_score}", 50),
+                    (vars.BORDER, vars.SCREEN_H // 2 + 50),
+                )
+                self.display.blit(
+                    drawText("R to restart", 50),
+                    (vars.BORDER, vars.SCREEN_H // 2 + 100),
+                )
+            if self.game_state == "Game Over":
+                self.display.blit(
+                    drawText("Game over!", 50), (vars.SCREEN_W // 2, vars.SCREEN_H // 2)
+                )
+                self.display.blit(
+                    drawText("R to restart", 50),
+                    (vars.SCREEN_W // 2, vars.SCREEN_H // 2 + 50),
+                )
 
-        if vars.DEBUG:
-            debug(gameState, 0)
-            debug(grid.mines_left, 40)
-            debug(pygame.mouse.get_pos(), 80)
-            debug(pygame.mouse.get_pressed(), 120)
+            if vars.DEBUG:
+                debug(self.game_state, 0)
+                debug(self.grid.mines_left, 40)
+                debug(pygame.mouse.get_pos(), 80)
+                debug(pygame.mouse.get_pressed(), 120)
 
-        # update screen
-        pygame.display.update()
-
-
-gameLoop()
-pygame.quit()
-quit()
+            # update screen
+            pygame.display.update()
---
+++ main.py
@@ -0,0 +1,13 @@
+from game import Game
+import pygame
+
+
+def main():
+    game = Game()
+    game.game_loop()
+    pygame.quit()
+    quit()
+
+
+if __name__ == "__main__":
+    main()
