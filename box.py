8fdd821a1386807cdb81c2c0190a0942f245f69b
miles <miles4599@gmail.com>
2022-05-18 14:14:03 -0700

    Made reveal_box and toggle_flag functions

--- box.py
+++ box.py
@@ -1,4 +1,5 @@
 from spritesheet import Spritesheet
+from gamestate import GameState
 import vars
 import pygame
 
@@ -24,43 +25,44 @@ class Box:
         self.flag = False  # Has player flagged the box
         self.sprites = Spritesheet.parse_box_sprites(Spritesheet)
 
-    def draw(self):
-        """
-        Draws a Box with a specific sprite depending on its value and its
-        boolean attributes mineFalse, clicked, mineClicked, and flag
-        """
-        surface = pygame.Surface((vars.BOX_SIZE, vars.BOX_SIZE))
-        if self.mineFalse:
-            surface.blit(self.sprites["box_no_bomb"], (0, 0))
-        else:
-            if self.clicked:
-                if self.val == -1:
-                    if self.mineClicked:
-                        surface.blit(self.sprites["box_red_bomb"], (0, 0))
-                    else:
-                        surface.blit(self.sprites["box_bomb"], (0, 0))
+
+def draw(self):
+    """
+    Draws a Box with a specific sprite depending on its value and its
+    boolean attributes mineFalse, clicked, mineClicked, and flag
+    """
+    surface = pygame.Surface((vars.BOX_SIZE, vars.BOX_SIZE))
+    if self.mineFalse:
+        surface.blit(self.sprites["box_no_bomb"], (0, 0))
+    else:
+        if self.clicked:
+            if self.val == -1:
+                if self.mineClicked:
+                    surface.blit(self.sprites["box_red_bomb"], (0, 0))
                 else:
-                    if self.val == 0:
-                        surface.blit(self.sprites["box_empty"], (0, 0))
-                    elif self.val == 1:
-                        surface.blit(self.sprites["box_1"], (0, 0))
-                    elif self.val == 2:
-                        surface.blit(self.sprites["box_2"], (0, 0))
-                    elif self.val == 3:
-                        surface.blit(self.sprites["box_3"], (0, 0))
-                    elif self.val == 4:
-                        surface.blit(self.sprites["box_4"], (0, 0))
-                    elif self.val == 5:
-                        surface.blit(self.sprites["box_5"], (0, 0))
-                    elif self.val == 6:
-                        surface.blit(self.sprites["box_6"], (0, 0))
-                    elif self.val == 7:
-                        surface.blit(self.sprites["box_7"], (0, 0))
-                    elif self.val == 8:
-                        surface.blit(self.sprites["box_8"], (0, 0))
+                    surface.blit(self.sprites["box_bomb"], (0, 0))
             else:
-                if self.flag:
-                    surface.blit(self.sprites["box_flag"], (0, 0))
-                else:
-                    surface.blit(self.sprites["box_full"], (0, 0))
-        return surface
+                if self.val == 0:
+                    surface.blit(self.sprites["box_empty"], (0, 0))
+                elif self.val == 1:
+                    surface.blit(self.sprites["box_1"], (0, 0))
+                elif self.val == 2:
+                    surface.blit(self.sprites["box_2"], (0, 0))
+                elif self.val == 3:
+                    surface.blit(self.sprites["box_3"], (0, 0))
+                elif self.val == 4:
+                    surface.blit(self.sprites["box_4"], (0, 0))
+                elif self.val == 5:
+                    surface.blit(self.sprites["box_5"], (0, 0))
+                elif self.val == 6:
+                    surface.blit(self.sprites["box_6"], (0, 0))
+                elif self.val == 7:
+                    surface.blit(self.sprites["box_7"], (0, 0))
+                elif self.val == 8:
+                    surface.blit(self.sprites["box_8"], (0, 0))
+        else:
+            if self.flag:
+                surface.blit(self.sprites["box_flag"], (0, 0))
+            else:
+                surface.blit(self.sprites["box_full"], (0, 0))
+    return surface
